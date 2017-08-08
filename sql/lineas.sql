7fdd86e2ea9991ef06ebe42ad4d21c72b6bf6551
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-08-08 11:34:48 +0200

    Dump on 08-08-2017 11:33:40 (#8)

--- sql/lineas.sql
+++ sql/lineas.sql
@@ -30,7 +30,6 @@ INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (40, '40', 'Plz. M
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (41, '41', 'Plz. Magadalena - Tablada', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (43, '43', 'Plz. Magdalena - El Tardon', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (52, '52', 'S. Bernardo - Palmete', 1, '#742929');
-INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (53, '53', 'Los Arcos Centro Penitenciario', 1, '#742929');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (87, 'B3', 'Gran Plaza - Santa Clara', 1, '#84c6e3');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (119, 'B4', 'San Bernardo - Torreblanca', 1, '#84c6e3');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (81, 'C1', 'Circular Exterior 1', 1, '#008431');
@@ -39,5 +38,6 @@ INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (83, 'C3', 'Circul
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (84, 'C4', 'Circular Interior 2', 1, '#008431');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (121, 'C5', 'Circular Centro 5', 1, '#008431');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (108, 'C6A', 'Circular Macarena Norte Sentido A', 1, '#008431');
+INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (109, 'C6B', 'Circular Macarena Norte Sentido B', 1, '#008431');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (100, 'EA', 'Aeropuerto', 1, '#84c6e3');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (60, 'T1', 'Metrocentro', 1, '#c60018');
