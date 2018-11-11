ff84dad0c91951002ac2f381da403a80f93ce23d
Rafa Vázquez <rafa.vazsan@gmail.com>
2018-11-11 23:04:33 +0100

    Dump on 11-11-2018 23:03:09

--- sql/lineas.sql
+++ sql/lineas.sql
@@ -6,7 +6,7 @@ INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (6, '06', 'Gta. S.
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (10, '10', 'Ponce de León - San Jerónimo', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (11, '11', 'Ponce de León - Los Principes', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (12, '12', 'Ponce de León -  Pino Montano', 1, '#000d6f');
-INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (13, '13', 'Plaza Duque Pino Montano', 1, '#000d6f');
+INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (13, '13', 'Plaza Duque Pino Montano ', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (14, '14', 'Plaza Duque - Poligono Norte - Las Golondrinas', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (15, '15', 'Ponce de León - San Diego', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (16, '16', 'Valdezorras - Rialto', 1, '#000d6f');
@@ -21,7 +21,7 @@ INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (28, '28', 'Prado 
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (29, '29', 'Prado S. Sebastian - Torreblanca', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (30, '30', 'Prado San Sebastián - La Paz', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (31, '31', 'Prado San Sebastián - Polígono Sur', 1, '#000d6f');
-INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (32, '32', 'Plz. Duque - Plg. Sur', 1, '#000d6f');
+INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (32, '32', 'Plz. Duque - Plg. Sur ', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (34, '34', 'Prado San Sebastián - Los Bermejales', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (37, '37', 'Pta. Jerez - Pedro Salvador - Bellavista', 1, '#000d6f');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (125, '38A', 'Prado - Pitamo - Olavide [d]', 1, '#000d6f');
@@ -36,9 +36,6 @@ INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (81, 'C1', 'Circul
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (82, 'C2', 'Circular Exterior 2', 1, '#008431');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (83, 'C3', 'Circular Interior 1', 1, '#008431');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (84, 'C4', 'Circular Interior 2', 1, '#008431');
-INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (121, 'C5', 'Circular Centro 5', 1, '#008431');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (108, 'C6A', 'Circular Macarena Norte Sentido A', 1, '#008431');
-INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (109, 'C6B', 'Circular Macarena Norte Sentido B [d]', 1, '#008431');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (100, 'EA', 'Aeropuerto [t][l]', 1, '#84c6e3');
-INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (71, 'LE', 'Prado San Sebastián - Sevilla Este [l]', 1, '#f7a800');
 INSERT INTO linea (id, numero, nombre, tipo_id, color) VALUES (60, 'T1', 'Metrocentro', 1, '#c60018');
